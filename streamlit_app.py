9e3bfddb52559bcd1780e39b22ad878f82755882
Midaenoch <138822913+Midaenoch@users.noreply.github.com>
2025-08-18 11:33:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ manual_input = {}
 if st.button("Predict"):
     try:
         if uploaded_file is not None:
-        input_data = uploaded_file_data[selected_features].copy()
+            input_data = uploaded_file_data[selected_features].copy()
 
         else:
             input_data = pd.DataFrame([manual_input])
@@ -61,4 +61,5 @@ if st.button("Predict"):
 
     except Exception as e:
         st.error(f"Prediction failed: {e}")
+
 
